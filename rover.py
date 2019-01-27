fbc1d82df42690969ed5af11ee20de6ca5491121
Guadalupe Natalia Ruiz <Laurana@Guadalupes-MacBook-Air.local>
2019-01-27 17:09:23 +0100

    Rovers are now initialized in rover class file

--- rover.py
+++ rover.py
@@ -41,3 +41,39 @@ class Rover():
         print(self.position,self.get_key(mathdir_rover, direction_dictionary))
 
         return self.position, self.get_key(mathdir_rover, direction_dictionary)
+
+############################################################################################################
+
+################## CREATE A ROVER WITH INSTRUCTIONS FROM INPUT FILE ##################
+
+rover_lines=[] #Creates an array with each line of the instructions file
+with open('rover.txt') as f:
+    for line in f:
+        rover_lines.append(line.strip())
+#print(rover_lines)
+
+################## CREATE ROVER 1 WITH INSTRUCTIONS FROM INPUT FILE ##################
+
+rover1 = [] # Vector for Rover 1: (x, y, direction)
+rover1[:0] = rover_lines[1].split(' ')
+
+rover1_position = np.array([int(rover1[0]),int(rover1[1])]) # (x,y) position (int) of Rover 1
+rover1_direction=[rover1[2]] # Direction (str) of Rover 1
+
+instructions_rover1 = [] #Gives a vector of strings with each instruction for Rover 1 as an element
+instructions_rover1[:0] = rover_lines[2]
+
+Rover_1=Rover(rover1_position, rover1_direction, instructions_rover1)
+
+################## CREATE ROVER 2 WITH INSTRUCTIONS FROM INPUT FILE ##################
+
+rover2= [] # Vector for Rover 2: (x, y, direction)
+rover2[:0] = rover_lines[3].split(' ')
+
+rover2_position=np.array([int(rover2[0]),int(rover2[1])]) # (x,y) position (int) of Rover 1
+rover2_direction=[rover2[2]] # Direction (str) of Rover 1
+
+instructions_rover2 = []  #Gives a vector of strings with each instruction for Rover 2 as an element
+instructions_rover2[:0] = rover_lines[4]
+
+Rover_2=Rover(rover2_position, rover2_direction, instructions_rover2)
