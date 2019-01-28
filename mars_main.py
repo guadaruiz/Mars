f07bca0955d05fa2d265ad98a0780554ff3bd86b
Guadalupe Natalia Ruiz <Laurana@Guadalupes-MacBook-Air.local>
2019-01-28 16:35:30 +0100

    explanatory comments included

--- mars_main.py
+++ mars_main.py
@@ -3,5 +3,5 @@
 from rover import Rover_1
 from rover import Rover_2
 
-Rover_1.move()
-Rover_2.move()
+Rover_1.move() #Calls the first rover and moves it according to supplied instructions.
+Rover_2.move() #Calls the second rover and moves it according to supplied instructions.
