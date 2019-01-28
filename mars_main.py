a83064de877f7adb56674128b6ec6bfe31ecc310
Guadalupe Natalia Ruiz <Laurana@Guadalupes-MacBook-Air.local>
2019-01-28 16:39:45 +0100

    explanatory comments added

--- mars_main.py
+++ mars_main.py
@@ -3,5 +3,7 @@
 from rover import Rover_1
 from rover import Rover_2
 
+''' This is the main file of the Mars Rover program.'''
+
 Rover_1.move() #Calls the first rover and moves it according to supplied instructions.
 Rover_2.move() #Calls the second rover and moves it according to supplied instructions.
